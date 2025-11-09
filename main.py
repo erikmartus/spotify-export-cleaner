3f115ce5c7355944bfbdc8575c5c70a38f082d4b
Erik Martus <erik@Eriks-MacBook-Pro.local>
2025-11-09 16:52:28 +0900

    write spotify export cleaner script

---
+++ main.py
@@ -0,0 +1,78 @@
+import inquirer
+import os
+from typing import List
+import json
+import pathlib
+
+EXPORTS_DIR = "exports"
+CLEANED_DIR = "cleaned"
+IGNORED_ARTIST_FILE = "ignored_artists.json"
+
+def main():
+    os.makedirs(EXPORTS_DIR, exist_ok=True)
+    os.makedirs(CLEANED_DIR, exist_ok=True)
+
+    streams = load_streaming_history()
+    ignored_artists = get_ignored_artists(streams)
+    clean_exported_data(ignored_artists)
+
+
+
+def load_streaming_history() -> List[dict]:
+    files = [EXPORTS_DIR + "/" + x for x in os.listdir(EXPORTS_DIR)]
+    streams = []
+
+    for file in files:
+        with open(file, "r") as f:
+            new_streams = json.load(f)
+            streams += [s for s in new_streams]
+            f.close()
+
+    return streams
+
+
+def get_ignored_artists(streams: List[dict]) -> List[str]:
+    try:
+        with open(IGNORED_ARTIST_FILE, "r") as f:
+            previously_ignored_artists = json.load(f)
+            f.close()
+    except FileNotFoundError:
+        previously_ignored_artists = []
+    artists = sorted({ s["artistName"] for s in streams })
+
+    questions = [
+        inquirer.Checkbox(
+            "ignored_artists",
+            message="Which artists would you like to ignore?",
+            choices=artists,
+            default=previously_ignored_artists,
+            carousel=True
+        )
+    ]
+    answer = inquirer.prompt(questions)
+    ignored_artists = answer.get("ignored_artists", [])
+
+    with open(IGNORED_ARTIST_FILE, "w") as f:
+        f.write(json.dumps(ignored_artists))
+
+    return ignored_artists
+
+
+def clean_exported_data(ignored_artist: List[str]):
+    files = [EXPORTS_DIR + "/" + x for x in os.listdir(EXPORTS_DIR)]
+
+    for file in files:
+        with open(file, "r", encoding="UTF-8") as f:
+            streams = json.load(f)
+            cleaned_streams = [s for s in streams if s["artistName"] not in ignored_artist]
+            cleaned_file_name = CLEANED_DIR + "/" + pathlib.Path(f.name).stem + "_cleaned.json"
+
+            with open(cleaned_file_name, "w") as cf:
+                cf.write(json.dumps(cleaned_streams))
+                cf.close()
+            
+            print(f"Cleaned {f.name} -> {cleaned_file_name}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Spotify Export Cleaner
+
+Interactive utility to cleanup Spotify streaming history before importing into Stats.fm
+
+## Usage
+
+1. Install poetry dependencies
+2. Add your exported JSON files to `/exports`
+3. Run `python3 main.py`
+4. Upload cleaned files in `/cleaned` to Stats.fm

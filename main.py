65ab1f859abcc8887fafbc052387f9634edf7a91
Erik Martus <erik@Eriks-MacBook-Pro.local>
2025-11-11 14:50:31 +0900

    adjust script for actual export data

--- main.py
+++ main.py
@@ -24,9 +24,15 @@ def load_streaming_history() -> List[dict]:
 
     for file in files:
         with open(file, "r") as f:
-            new_streams = json.load(f)
-            streams += [s for s in new_streams]
-            f.close()
+            try:
+                new_streams = json.load(f)
+                streams += [
+                    s for s in new_streams
+                    if s["master_metadata_track_name"] is not None # filter out podcasts and audiobooks not tracked by Stats.fm
+                ]
+                f.close()
+            except Exception as e:
+                print(f"Error loading streams from {f.name}: {str(e)}")
 
     return streams
 
@@ -38,12 +44,12 @@ def get_ignored_artists(streams: List[dict]) -> List[str]:
             f.close()
     except FileNotFoundError:
         previously_ignored_artists = []
-    artists = sorted({ s["artistName"] for s in streams })
+    artists = sorted({ s["master_metadata_album_artist_name"] for s in streams }, key=lambda x: x.lower())
 
     questions = [
         inquirer.Checkbox(
             "ignored_artists",
-            message="Which artists would you like to ignore?",
+            message=f"Which artists would you like to ignore? ({len(artists)} found, {len(previously_ignored_artists)} previously ignored)",
             choices=artists,
             default=previously_ignored_artists,
             carousel=True
@@ -64,14 +70,16 @@ def clean_exported_data(ignored_artist: List[str]):
     for file in files:
         with open(file, "r", encoding="UTF-8") as f:
             streams = json.load(f)
-            cleaned_streams = [s for s in streams if s["artistName"] not in ignored_artist]
+            cleaned_streams = [s for s in streams if s["master_metadata_album_artist_name"] not in ignored_artist]
+            filtered_streams = [s for s in streams if s["master_metadata_album_artist_name"] in ignored_artist]
+            
             cleaned_file_name = CLEANED_DIR + "/" + pathlib.Path(f.name).stem + "_cleaned.json"
 
             with open(cleaned_file_name, "w") as cf:
                 cf.write(json.dumps(cleaned_streams))
                 cf.close()
             
-            print(f"Cleaned {f.name} -> {cleaned_file_name}")
+            print(f"Cleaned {f.name} -> {cleaned_file_name} ({len(filtered_streams)} of {len(streams)}  streams removed)")
 
 
 if __name__ == "__main__":
